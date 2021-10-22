8881d28294b565633c8624dde520a6458d4a10b4
Júlia Pérez Hofland <57637217+JuliaHofland@users.noreply.github.com>
2021-10-22 17:39:08 +0200

    Add files via upload

---
+++ src/main/java/CalendarProject/Event.java
@@ -0,0 +1,49 @@
+package CalendarProject;
+import java.util.Date;
+
+
+public class Event {
+    private String name;
+    private String desc;
+    private int priority;
+    private Date date;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+
+
+
+
+
+
+}
